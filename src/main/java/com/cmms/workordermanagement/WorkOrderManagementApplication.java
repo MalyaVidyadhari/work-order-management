6b5d858e8ec6d06469a054970e7257d3a21d726a
Malya Vidyadhari Pechetti <pechetti.malya@gmail.com>
2025-01-02 22:01:35 +0530

    register service first commit

---
+++ src/main/java/com/cmms/workordermanagement/WorkOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.cmms.workordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkOrderManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkOrderManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmms/workordermanagement/WorkOrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmms.workordermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkOrderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
